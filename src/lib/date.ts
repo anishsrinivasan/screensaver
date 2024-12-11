8ea96e814060e473d18729924727404214f8f028
Anish K Srinivasan <itsanishsrinivasan@gmail.com>
2024-12-11 19:13:53 +0530

    Update src/lib/date.ts [BoltSync]

---
+++ src/lib/date.ts
@@ -0,0 +1,10 @@
+/**
+ * Formats a date object into a 12-hour time string (hh:mm AM/PM)
+ */
+export function formatTime(date: Date): string {
+  return new Intl.DateTimeFormat('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true,
+  }).format(date);
+}
